16e4bf7108c63fa4d35f072cf63d05b0c960cac7
rathodvinod5 <rathodvinod5@gmail.com>
2026-05-13 22:33:03 +0530

    added burn_tokens instruction set

--- programs/solana-stablecoin/src/instructions/burn_tokens.rs
+++ programs/solana-stablecoin/src/instructions/burn_tokens.rs
@@ -1,11 +1,30 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    associated_token::AssociatedToken, token_2022::Token2022, token_interface::{
+    associated_token::AssociatedToken,  token_2022::{burn, Burn, Token2022}, token_interface::{
         Mint, TokenAccount
     }
 };
 
 use crate::states::Config;
+
+pub fn burn_tokens(ctx: Context<BurnTokens>, amount: u64) -> Result<()> {
+
+    let burn_accounts = Burn {
+        mint: ctx.accounts.mint.to_account_info(),
+        from: ctx.accounts.owner_ata.to_account_info(),
+        authority: ctx.accounts.owner.to_account_info(),
+    };
+    let burn_cpi_context = CpiContext::new(
+        ctx.accounts.token_program.to_account_info(),
+        burn_accounts
+    );
+
+    let _ = burn(burn_cpi_context, amount)?;
+
+    msg!("Burned {} tokens from {}", amount, ctx.accounts.owner_ata.key());
+
+    Ok(())
+}
 
 
 #[derive(Accounts)]
@@ -14,7 +33,8 @@ pub struct BurnTokens<'info> {
     pub owner: Signer<'info>,
 
     #[account(
-        // mut,
+        mut,
+        has_one = mint,
         seeds = [b"config"],
         bump = config.config_bump,
     )]
